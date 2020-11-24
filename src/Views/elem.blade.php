60b1f3b22a79a911687d7493cfa5a4db7d3672c1
Regnilk <jack@regnilk.fr>
2020-11-24 09:01:26 +0100

    update elem structure

--- src/Views/elem.blade.php
+++ src/Views/elem.blade.php
@@ -1,8 +1,8 @@
 <div class="form-group{{ $errors->has($name) ? ' has-error' : '' }}">
-    <div class="col-md-{{$width}}">
+    <div class="px-0">
         <div {{ $attributes->merge(['class' => "input-group"])}}>
             <div class="input-group-prepend">
-                <label class="input-group-text d-inline-block pl-1 text-left" id="{{$name}}-label" style="width: {{$labelWidth}}px;">
+                <label class="input-group-text" id="{{$name}}-label" style="min-width: {{$labelWidth}}px;">
                     <x-fa icon="{{$icon}}" class="mr-1"/>{{$label}}
                 </label>
             </div>
